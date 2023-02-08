c021d2f9f5125561110a60e6e76c71e16f736bf9
hoonah137 <121156401+hoonah137@users.noreply.github.com>
2023-02-08 10:25:48 +0100

    sdf

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 0.5 f;
+
     void Start()
     {
         
@@ -13,7 +15,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+         rBody.velocity = new Vector2(horizontal * speed, rBody.velocity.y);
+
         
+    }
+
+    public void Die ()
+    {
+       
     }
 
     void OnCollisionEnter2D(Collision2D collision) 
@@ -23,5 +33,20 @@ public class Enemy : MonoBehaviour
             Debug.Log("ESO TE PASA POR INTENTAR MATAR A MIS GOOMBITAS!!!");
             Destroy(collision.gameObject);
         }
+
+        if(collision.gameObject.tag == "noGoomba")
+        {
+            if (horizontal == 1)
+            {
+                horizontal = -1;
+            }
+            else 
+            {
+                horizontal = 1;
+            }
+
+            
+        }
+        
     }
 }
